58359c0abc7baaa94d91546b4ea9d0c55f186807
Vanya <vanya.tran@techvify.com.vn>
2025-02-17 09:43:31 +0700

    fix type error

--- src/types/declaration.d.ts
+++ src/types/declaration.d.ts
@@ -2,3 +2,11 @@ declare module "*.svg" {
   const content: string;
   export default content;
 }
+
+interface ImportMetaEnv {
+  readonly VITE_LINK_EXCEL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
