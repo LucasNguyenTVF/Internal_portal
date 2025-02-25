f89fa40e0e9c884975e966bec5f57b9f05485a6e
Lucas Nguyen <lucas.nguyen@techvify.com.vn>
2025-02-25 10:46:35 +0700

    refactor: simplify onClickDetail function and enhance employee info display

--- src/components/information/Employee.tsx
+++ src/components/information/Employee.tsx
@@ -38,40 +38,38 @@ export const DisplayEmployeeInfo = (
   getInitials: (name: string) => string
 ): React.JSX.Element => {
   const detailModalRef = useRef<ModalRef>(null);
-
-  const onClickDetail = useCallback(
-    (data: EmployeeData) => () => {
-      detailModalRef?.current?.showModal(data);
-    },
-    []
-  );
-
+  const onClickDetail = (data) => {
+    detailModalRef?.current?.showModal(data);
+  };
   return (
-    <div
-      key={employee.id}
-      className="bg-[linear-gradient(90deg,_#03081A_0%,_#012756_100%)] border shadow-md border-gray-800 rounded-lg px-1.5 py-2 hover:bg-gray-750 transition-colors w-full sm:w-auto"
-      onClick={() => onClickDetail(employee)}
-    >
-      <div className="flex flex-col sm:flex-row items-center justify-between gap-2">
-        <div className="sm:w-auto">
-          <Avatar
-            src={employee.image}
-            alt={employee.fullName}
-            fallback={getInitials(employee.fullName)}
-          />
-        </div>
+    <>
+      <div
+        key={employee.id}
+        className="bg-[linear-gradient(90deg,_#03081A_0%,_#012756_100%)]  cursor-pointer border shadow-md border-gray-800 rounded-lg px-1.5 py-2 hover:bg-gray-750 transition-colors w-full sm:w-auto"
+        onClick={() => onClickDetail(employee)}
+      >
+        <div className="flex flex-col sm:flex-row items-center justify-between gap-2">
+          <div className="sm:w-auto">
+            <Avatar
+              src={employee.image}
+              alt={employee.fullName}
+              fallback={getInitials(employee.fullName)}
+            />
+          </div>
 
-        <div className="min-w-0 flex-1 w-full">
-          <div className="flex flex-col items-center sm:items-start text-center sm:text-left">
-            <span className="text-xs text-white truncate w-full">
-              {employee.account}
-            </span>
-            <p className="text-[10px] text-[#2A88FF] truncate w-full">
-              {employee.position}
-            </p>
+          <div className="min-w-0 flex-1 w-full">
+            <div className="flex flex-col items-center sm:items-start text-center sm:text-left">
+              <span className="text-xs text-white truncate w-full">
+                {employee.account}
+              </span>
+              <p className="text-[10px] text-[#2A88FF] truncate w-full">
+                {employee.position}
+              </p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      <InfoDetailModal ref={detailModalRef} />
+    </>
   );
 };
